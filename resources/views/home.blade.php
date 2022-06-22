58b77d0072e7accede9dd5a543b1cb1e1abedacc
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-22 16:51:01 +0700

    #6  update mobile responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<section class = "banner">
-    <div class="banner-container">
+<section class="banner">
+    <div class="container-fluid banner-main">
         <div class="banner-content">
             <div class="banner-heading">
                 <p class="banner-slogan">
                     Learn Anytime, Anywhere
-                    <span class="slogan-title">at Hapo Learn <img src="{{ asset('images/cu.png') }}" alt="At Hapo"> !</span>
+                    <span class="slogan-title">at Hapo Learn <img src="{{ asset('images/owl.png') }}" alt="At Hapo"> !</span>
                 </p>
             </div>
             <div class="banner-text">
@@ -21,57 +21,6 @@
             </button>
         </div>
     </div>
-    <div class="bg"></div>
+    <div class="bg-banner"></div>
 </section>
-<section class="container">
-    <div class="row">
-        <div class="course one col-lg-4">
-            <div class="card">
-                <div class="row no-gutters">
-                    <div class="card-img-top col-lg-12 col-md-4">
-                        <div class="logo"></div>
-                    </div>
-                    <div class="card-body col-lg-12 col-md-8">
-                        <h5 class="card-title">PHP Tutorial</h5>
-                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                        <a href="#" class="btn btn-hapo">Take This Course</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="course two col-lg-4">
-            <div class="card">
-                <div class="row no-gutters">
-                    <div class="card-img-top col-lg-12 col-md-4">
-                        <div class="logo"></div>
-                    </div>
-                    <div class="card-body col-lg-12 col-md-8">
-                        <h5 class="card-title">PHP Tutorial</h5>
-                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I
-                            had
-                            coded quite a bit, but never touched anything in regards to web development.</p>
-                        <a href="#" class="btn btn-hapo">Take This Course</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="course three col-lg-4">
-            <div class="card">
-                <div class="row no-gutters">
-                    <div class="card-img-top col-lg-12 col-md-4">
-                        <div class="logo"></div>
-                    </div>
-                    <div class="card-body col-lg-12 col-md-8">
-                        <h5 class="card-title">PHP Tutorial</h5>
-                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS before entering New Media. I
-                            had
-                            coded quite a bit, but never touched anything in regards to web development.</p>
-                        <a href="#" class="btn btn-hapo">Take This Course</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
 @endsection
