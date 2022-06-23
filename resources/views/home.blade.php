feeac633222dc81aaedb48d9298229e2031ee0ce
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-23 17:15:51 +0700

    #3 test responsive mb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,30 +2,31 @@
 
 @section('content')
 
-<section class="example background-banner"></section>
-<div class="container-fluid">
-    <div class="row row-color">
-        <div class="col-md-5 title">
-            <div class="banner-title">
-                Learn Anytime, Anywhere
-                <div class="title-extra">
-                    at HapoLearn
-                    <img src="/images/owl.png" alt="haposhoft" class="title-img">
+<section class="background-banner">
+    <div class="container-fluid">
+        <div class="row row-color">
+            <div class="col-md-5 title">
+                <div class="banner-title">
+                    Learn Anytime, Anywhere
+                    <div class="title-extra">
+                        at HapoLearn
+                        <img src="/images/owl.png" alt="haposhoft" class="title-img">
                         !
+                    </div>
                 </div>
-            </p>
-        </div>
-        <div class="title-content">
-            Interactive lessons, "on the go" 
-            <div class= "title-content--extra">
-                practice, peer support
+                <div class="title-content">
+                    Interactive lessons, "on the go" 
+                    <div class= "title-content--extra">
+                        practice, peer support
+                    </div>
+                </div>
+                <div class="start">
+                    <button class="btn-start">
+                        Start Learning Now!
+                    </button>
+                </div>
             </div>
         </div>
-        <div class="start">
-            <button class="btn-start">
-                Start Learning Now!
-            </button>
-        </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,15 @@
 <header class="main-header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-order">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-block">
         <a class="navbar-brand navbar-img" href="#">
             <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
         </a>
         <button class="navbar-toggler nav-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon nav_icon"></span>
+            <span class="navbar-toggler-icon nav-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav  w-100 mr-44px">
+            <ul class="navbar-nav w-100 mr-44">
                 <li class="nav-item ">
-                    <a class="nav-link" href="#">Home </a>
+                    <a class="nav-link" href="#">home </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">all courses</a>
