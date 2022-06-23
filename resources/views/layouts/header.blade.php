3a60ffffe510d9ed26a9b88fd5ad2f6cb55e8cba
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-23 09:20:12 +0700

    #6 update layout & classname banner,course

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,24 +1,26 @@
 <header class="main-header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light bg-white flex-sb nav-test">
-        <a class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-order">
+        <a class="navbar-brand navbar-img" href="#">
             <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler nav-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon nav_icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto w-100">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+            <ul class="navbar-nav  w-100 mr-44px">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Home </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">ALL COURSES</a>
+                    <a class="nav-link" href="#">All COURSES</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link " href="#"  aria-haspopup="true" aria-expanded="false">
+                        LOGIN/REGISTER
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">LOGIN/REGISTER</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">PROFILE</a>
+                    <a class="nav-link " href="#">PROFILE</a>
                 </li>
             </ul>
         </div>
