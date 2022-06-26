ebb5d80690741697106e09784bd5d4a92f697c29
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-26 22:54:59 +0700

    #6 fix ÈOL slick.js

--- resources/js/slick.js
+++ resources/js/slick.js
@@ -1,19 +1,19 @@
-
-  $('.slider').slick({
-      slidesToShow: 2,
-      prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-      nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-      responsive: [{
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-          }
-        },
-        {
-          breakpoint: 739,
-          settings: {
-            slidesToShow: 1,
-          }
+$('.slider').slick({
+    slidesToShow: 2,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 1,
         }
-      ]
-  });
+    },
+    {
+        breakpoint: 739,
+        settings: {
+        slidesToShow: 1,
+        }
+    }
+    ]
+});
+  
