cdbbf7d92daae651c36ebad9371dce7ffdbf60ba
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-27 14:42:29 +0700

    #8 update layout home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,33 @@
 
 @section('content')
 
-<section class="background-banner"></section>
-<div class="container-fluid">
-    <div class="row row-color">
-        <div class="col-md-5 title">
-            <div class="banner-title">
-                Learn Anytime, Anywhere
-                <div class="title-extra">
-                    at HapoLearn
-                    <img src="/images/owl.png" alt="haposhoft" class="title-img">
-                    !
+<section class="background-banner">
+    <div class="container-fluid">
+        <div class="row row-color">
+            <div class="col-md-5 title">
+                <div class="banner-title">
+                    Learn Anytime, Anywhere
+                    <div class="title-extra">
+                        at HapoLearn
+                        <img src="/images/owl.png" alt="haposhoft" class="title-img">
+                        !
+                    </div>
                 </div>
-            </div>
-            <div class="title-content">
-                Interactive lessons, "on the go" 
-                <div class= "title-content--extra">
-                    practice, peer support
+                <div class="title-content">
+                    Interactive lessons, "on the go" 
+                    <div class= "title-content--extra">
+                        practice, peer support
+                    </div>
                 </div>
-            </div>
-            <div class="start">
-                <button class="btn-start">
-                    Start Learning Now!
-                </button>
+                <div class="start">
+                    <button class="btn-start">
+                        Start Learning Now!
+                    </button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 <section class="course">
     <div class="container">
         <div class="row ">
