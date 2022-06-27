43370f9b463ef519717d1043111cfb1ec407e667
toanmu10 <62554809+toanmu10@users.noreply.github.com>
2022-06-27 12:20:41 +0700

    Revert "#6 add course and responsive "

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <section class="background-banner"></section>
 <div class="container-fluid">
-    <div class="row">
+    <div class="row row-color">
         <div class="col-md-5 title">
             <div class="banner-title">
                 Learn Anytime, Anywhere
@@ -24,103 +24,6 @@
                 <button class="btn-start">
                     Start Learning Now!
                 </button>
-            </div>
-        </div>
-    </div>
-</div>
-<section class="course">
-    <div class="container">
-        <div class="row ">
-            <div class="col-lg-4 card-distance mt-40 ">
-                <div class="card border-course card-position card-shadow" >
-                    <div class="row no-gutters">
-                        <div class="img-custom card-one col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >HTML/CSS/js Tutorial</h5>
-                            <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 card-distance">
-                <div class="card border-course card-position card-shadow" >
-                    <div class="row no-gutters">
-                        <div class="img-custom card-two col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >LARAVEL Tutorial</h5>
-                            <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 card-distance card-end ">
-                <div class="card border-course card-position card-shadow" >
-                    <div class="row no-gutters">
-                        <div class="img-custom card-three col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >PHP Tutorial</h5>
-                            <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="start">
-                <button class="btn-start">
-                    Start Learning Now!
-                </button>
-            </div>
-        </div>
-        <!-- Other courses -->
-        <div class="row justify-content-center">
-            <div class="col-lg-6 other-courses-title">
-                <p class="other-courses-text">Other courses</p>
-            </div>
-        </div>
-        <div class="row other-card-position ">
-            <div class="col-lg-4 card-distance">
-                <div class="card border-course card-shadow" >
-                    <div class="row no-gutters">
-                        <div class="img-custom card-one col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >CSS Tutorial</h5>
-                            <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 card-distance">
-                <div class="card border-course card-shadow" >
-                    <div class="row no-gutters">
-                    <div class="img-custom card-two col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >Ruby on rails Tutorial</h5>
-                            <p class="card-text title-course-content"> I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 card-distance card-end">
-                <div class="card border-course card-shadow" >
-                    <div class="row no-gutters">
-                        <div class="img-custom card-three col-lg-12 col-md-4 border-radius-img"></div>
-                        <div class="card-body col-lg-12 col-md-8 card-content">
-                            <h5 class="card-title title-course" >Ruby on rails Tutorial</h5>
-                            <p class="card-text title-course-content"> I knew hardly anything about HTML, JS, and CSS before entering New Media,...</p>
-                            <a href="#" class="btn btn-course">Take This Course</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-lg-5 course-shows">
-                View All Our Courses 
-                <i class="fa-solid fa-arrow-right"></i>
             </div>
         </div>
     </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav w-100 navbar-list">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home </a>
+                    <a class="nav-link" href="#">home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">all courses</a>
