bfcb5bfeea27e7719a19519f8d7e09186c7559ff
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-28 11:58:02 +0700

    fix layout in banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,32 +3,32 @@
 @section('content')
 
 <section class="background-banner">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-5 title">
-                <div class="banner-title">
-                    Learn Anytime, Anywhere
-                    <div class="title-extra">
-                        at HapoLearn
-                        <img src="/images/owl.png" alt="haposhoft" class="title-img">
-                        !
-                    </div>
+</section>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-5 title">
+            <div class="banner-title">
+                Learn Anytime, Anywhere
+                <div class="title-extra">
+                    at HapoLearn
+                    <img src="/images/owl.png" alt="haposhoft" class="title-img">
+                    !
                 </div>
-                <div class="title-content">
-                    Interactive lessons, "on the go" 
-                    <div class= "title-content--extra">
-                        practice, peer support
-                    </div>
+            </div>
+            <div class="title-content">
+                Interactive lessons, "on the go" 
+                <div class= "title-content--extra">
+                    practice, peer support
                 </div>
-                <div class="start">
-                    <button class="btn-start">
-                        Start Learning Now!
-                    </button>
-                </div>
+            </div>
+            <div class="start">
+                <button class="btn-start">
+                    Start Learning Now!
+                </button>
             </div>
         </div>
     </div>
-</section>
+</div>
 <section class="course">
     <div class="container">
         <div class="row ">
