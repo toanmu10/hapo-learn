deb9d30a90c82b4f50f7c62c3f53f0047021391d
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-06-28 09:57:40 +0700

    #12 delete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,7 +59,7 @@
             <div class="col-lg-4 card-distance card-end ">
                 <div class="card border-course card-position card-shadow" >
                     <div class="row no-gutters">
-                        <div class="img-custom card-three col-lg-12 col-md-4 border-radius-img"></div>
+                        <div class="img-custom card-three col-lg-12 col-md-4 rounded-top-1"></div>
                         <div class="card-body col-lg-12 col-md-8 card-content">
                             <h5 class="card-title title-course" >PHP Tutorial</h5>
                             <p class="card-text title-course-content">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
