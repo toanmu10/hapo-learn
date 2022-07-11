1d44398a9b5f6630457254c4d36ebb42a529571d
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-07-11 16:06:25 +0700

    #18 delete comment

--- database/migrations/2022_07_10_101203_create_programs_table.php
+++ database/migrations/2022_07_10_101203_create_programs_table.php
@@ -20,11 +20,6 @@ class CreateProgramsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('programs');
