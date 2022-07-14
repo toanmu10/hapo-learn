67ce458dad335cf5968c2bdedd045f2791b3674e
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-07-14 09:46:27 +0700

    #21 fix convention

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -33,7 +33,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'username.required' => 'Vui lòng nhập username',
-            'username.max' => 'Username có tối đa nhất :min kí tự',
+            'username.max' => 'Username có tối đa nhất :max kí tự',
             'password.required' => 'Vui lòng nhập password',
             'password.min' => 'Password có ít nhất :min kí tự',
         ];
