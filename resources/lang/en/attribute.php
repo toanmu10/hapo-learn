89030c579a897eb07b342a1a61c63bc05cc3b012
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-07-19 15:32:29 +0700

    #23 attribute to En

---
+++ resources/lang/en/attribute.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'sign_up_to_hapolearn' => 'Sign up to HapoLearn',
+    'sign_up' => 'Sign Up',
+    'username' => 'Username',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'sign_in_to_hapolearn' => 'Sign in to HapoLearn',
+    'sign_in' => 'Sign In',
+    'forgot_your_password' => 'Forgot Your Password',
+    'sign_in_with' => 'Sign In With',
+    'or_new_to_hapolearn' => 'Or New to HapoLearn',
+    'create_new_account' => 'Create new account',
+];
