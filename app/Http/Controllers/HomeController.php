44feba455cef52c7d8c896a41dbd4b5594aea767
toanmu10 <nguyenanhtoan6666@gmail.com>
2022-07-21 12:58:20 +0700

    #26 use scope and variable

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,13 +10,13 @@ use App\Models\UserCourse;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->course = new Course();
-        $this->review = new Review();
-        $this->lesson = new Lesson();
-        $this->userCourse = new UserCourse();
-    }
+    // public function __construct()
+    // {
+    //     $this->course = new Course();
+    //     $this->review = new Review();
+    //     $this->lesson = new Lesson();
+    //     $this->userCourse = new UserCourse();
+    // }
     /**
      * Show the application dashboard.
      *
@@ -24,14 +24,28 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', [
-            'courses' => $this->course->showCourses(3)->get(),
-            'otherCourses' => $this->course->showOtherCourses(3)->orderByField('name', 'asc')->get(),
-            'reviews' => $this->review->showReviews(6)->get(),
-            'countCourses' => $this->course->countCourses()->get()->count(),
-            'countLessons' => $this->lesson->countLessons()->get()->count(),
-            'countUsers' => $this->userCourse->countLearners('user_id')->groupByField('user_id')->get()->count()
-        ]);
+        // return view('home', [
+        //     'courses' => $this->course->showCourses(config('course.number_course_in_home_blade'))->get(),
+        //     'otherCourses' => $this->course->showOtherCourses(3)->orderByField('name', 'asc')->get(),
+        //     'reviews' => $this->review->showReviews(6)->get(),
+        //     'countCourses' => $this->course->countCourses()->get()->count(),
+        //     'countLessons' => $this->lesson->countLessons()->get()->count(),
+        //     'countUsers' => $this->userCourse->countLearners('user_id')->groupByField('user_id')->get()->count()
+        // ]);
+
+        $courses = Course::showCourses(config('course.number_course'))->get();
+
+        $otherCourses = Course::showOtherCourses(config('course.number_course'), config('course.sort_low_to_high'))->get();
+
+        $countLessons = Lesson::countLessons()->count();
+
+        $countCourses = Course::countCourses()->count();
+
+        $countUsers = UserCourse::countLearners(config('user_course.select_field'))->groupByField(config('user_course.group_by_field'))->get()->count();
+
+        $reviews = Review::showReviews(config('review.number_review'))->get();
+
+        return view('home')->with(compact('courses', 'otherCourses', 'countLessons', 'countCourses', 'countUsers', 'reviews'));
     }
 
     public function test()
---
+++ config/review.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'number_review' => 6
+];
